5348f9db2e67105812cbe1da21171d6dd77acb8c
bramjanssens <bram.janssens@infosupport.com>
2019-11-18 16:14:38 +0100

    Module second --> secondauto.

--- modularity/2-buildWithClasspathDependency.cmd
+++ modularity/2-buildWithClasspathDependency.cmd
@@ -13,15 +13,15 @@ del sources.txt
 mkdir output\classes
 dir /s /B second\*.java > sources.txt
 javac -d output/classes -cp output\mlib\first.jar @sources.txt
-jar -c -f output/mlib/second.jar -C output/classes .
+jar -c -f output/mlib/secondauto.jar -C output/classes .
 rmdir /Q /S output\classes
 del sources.txt
 
 echo -------------- Running in the classpath ------------------------------------
-java -cp output\mlib\first.jar;output\mlib\second.jar com.example.second.Second rem now with -cp
+java -cp output\mlib\first.jar;output\mlib\secondauto.jar com.example.second.Second 
 
 echo -------------  Running in the modulepath using automatic module name -------
-java -p output\mlib -m second/com.example.second.Second
+java -p output\mlib -m secondauto/com.example.second.Second
 
 REM when running classic jar in the modulepath, jar becomes module with automatic name == jar name
 REM for java.exe prefix main class with automatic module name
@@ -30,4 +30,4 @@ REM -d examines metadata of module graph (2b)
 echo --- Module data of first is: ---
 jar -f output\mlib\first.jar -d 
 echo --- Module data of second is: ---
-jar -f output\mlib\second.jar -d 
+jar -f output\mlib\secondauto.jar -d 
